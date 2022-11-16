619e0ff6ed15d31417f2818ac2a61dd01603eab1
avakhtina <avakhtina@phoenixit.ru>
2022-11-16 23:44:42 +0300

    fix l3ex1

--- short_phrase.py
+++ short_phrase.py
@@ -2,4 +2,4 @@ class TestExample:
     def test_size(self):
         expected_result = 15
         phrase = input("Set a phrase: ")
-        assert len(phrase) == expected_result, f"expected size of phrase is {expected_result}"
+        assert len(phrase) < expected_result, f"expected size of phrase less then {expected_result}"
